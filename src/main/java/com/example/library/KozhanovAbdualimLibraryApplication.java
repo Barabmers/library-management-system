46bc2838e38a926528bf3777484a81043964e604
kozhanov abdualim <bubanovm@gmail.com>
2026-05-10 00:02:05 +0500

    initial spring boot project setup

---
+++ src/main/java/com/example/library/KozhanovAbdualimLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KozhanovAbdualimLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KozhanovAbdualimLibraryApplication.class, args);
+    }
+
+}
