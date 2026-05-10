4e2f92787eeaab55591d45c35ad8ca6cdd9bbadd
kozhanov abdualim <bubanovm@gmail.com>
2026-05-10 18:11:36 +0500

    add author repository

---
+++ src/main/java/com/example/library/repository/KozhanovAbdualimAuthorRepository.java
@@ -0,0 +1,10 @@
+package com.example.library.repository;
+
+import com.example.library.entity.KozhanovAbdualimAuthor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface KozhanovAbdualimAuthorRepository extends JpaRepository<KozhanovAbdualimAuthor, Long> {
+
+}
