2b4eab6882628b09bde11894e0acc177697f53c1
kozhanov abdualim <bubanovm@gmail.com>
2026-05-10 16:53:33 +0500

    add author entity

---
+++ src/main/java/com/example/library/entity/KozhanovAbdualimAuthor.java
@@ -0,0 +1,29 @@
+package com.example.library.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "authors")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class KozhanovAbdualimAuthor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(columnDefinition = "TEXT")
+    private String bio;
+}
