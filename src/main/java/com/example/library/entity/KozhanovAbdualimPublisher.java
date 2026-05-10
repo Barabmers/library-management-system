99c7c212c21db501ec0295b9803d3b6208f9bed3
kozhanov abdualim <bubanovm@gmail.com>
2026-05-10 16:56:12 +0500

    add publisher entity

---
+++ src/main/java/com/example/library/entity/KozhanovAbdualimPublisher.java
@@ -0,0 +1,26 @@
+package com.example.library.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "publishers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class KozhanovAbdualimPublisher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(length = 200)
+    private String address;
+}
